4cff5b582510b4ebe5e093718607b41a857e23a7
Lars <drumberhd@gmail.com>
2020-07-14 17:01:41 +0200

    added swipe to open gesture for side menu

--- js/main.js
+++ js/main.js
@@ -1,14 +1,33 @@
 jQuery(document).ready(function ($) {
 
     $("#nav-toggle").click(function() {
-        console.log($("#nav-toggle").data("toggle"));
         if($(this).data("toggle") === "collapse") {
-            $(this).data("toggle", "expand");
-            $("#nav-list").addClass("expand");
+            openSideMenu();
         } else {
-            $(this).data("toggle", "collapse");
-            $("#nav-list").removeClass("expand");
+            closeSideMenu();
         }
     });
     
+});
+
+function openSideMenu() {
+    $("#nav-toggle").data("toggle", "expand");
+    $("#nav-list").addClass("expand");
+};
+
+function closeSideMenu() {
+    $("#nav-toggle").data("toggle", "collapse");
+    $("#nav-list").removeClass("expand");
+};
+
+$(document).swipe({
+    threshold:150,
+    maxTimeThreshold:1000,
+    swipe:function(event, direction, distance, duration, fingerCount)
+        {
+            if(direction == "right")
+                openSideMenu();
+            else if(direction == "left")
+                closeSideMenu();
+        }
 });
