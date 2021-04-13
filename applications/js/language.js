3abad536a43a7ca6983a7b5ee7078bafe6f50cc6
TomateX <thomasekhardt36@gmail.com>
2021-04-13 17:57:58 +0200

    chage the main site and add 2 new Websites
    change BlackForestApplications to BlackForestHolding
    add BlackforestGames and BlackForestApplications
    to change the site cklick on the menu
    to go back to the main site, click on the BFH icon on the left top corner

---
+++ applications/js/language.js
@@ -0,0 +1,86 @@
+/*
+    add new translation files here \/
+*/
+const LANGS = {
+    "en": "./translations/en.json",
+    "es": "./translations/es.json",
+    "fr": "./translations/fr.json",
+    "ru": "./translations/ru.json",
+    "ja": "./translations/ja.json"
+}
+// selected language
+var lang = "de";
+
+jQuery(document).ready(function ($) {
+
+    var userLanguage = getUserLanguage();
+    lang = userLanguage;
+
+    if(typeof(Storage) !== "undefined" && localStorage != null) {
+        if(localStorage.getItem("language")) {
+            lang = localStorage.getItem("language");
+        }
+    }
+
+    changeLanguage();
+
+});
+
+function changeLanguage() {
+    // if translation of language is not available (and it is not german),
+    // then set language to english as default
+    if(!LANGS[lang] && lang !== "de") {
+        lang = "en";
+    }
+
+    // get path to language file
+    let langFile = LANGS[lang];
+    // if object entry exists, get the json data
+    if(langFile) {
+        // load json file
+        $.getJSON(langFile, function(json) {
+            // loop through all elements which have 'data-lang' attribute
+            $("[data-lang]").each(function() {
+                // get the translation id of the element
+                let id = this.attributes.getNamedItem("data-lang").value;
+                // get the corresponding json value (= translation)
+                let translation = json[id];
+
+                if(translation) {
+                    // replace text of the element
+                    this.innerHTML = translation;
+                }
+        
+            });
+            
+        });
+
+        // set current language as selected
+        $("#language-select")[0].value = lang;
+
+    }
+
+    // reveal text content
+    setTimeout(function() {
+        uncensor();
+    }, 50);
+}
+
+function setLanguage(langCode) {
+    lang = langCode;
+    if(typeof(Storage) !== "undefined" && localStorage != null) {
+        localStorage.setItem("language", lang);
+    }
+    changeLanguage();
+    if(langCode === "de")
+        location.reload();
+}
+
+function getUserLanguage() {
+    let userLang = navigator.language || navigator.userLanguage;
+    // if some region is specified, return only the lang code
+    if(userLang.indexOf('-') !== -1) {
+        userLang = userLang.substring(0, 2);
+    }
+    return userLang;
+}
